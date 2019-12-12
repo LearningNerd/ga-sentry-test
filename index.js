f250ad1d2abb62864419954776a47e08bcb646c2
Liz Krane <learningnerd@gmail.com>
2019-12-12 11:54:07 -0800

    test

---
+++ index.js
@@ -0,0 +1,32 @@
+const core = require('@actions/core');
+const SentryCli = require('@sentry/cli');
+
+runAction().catch( error => {
+  console.log(error.message);
+  core.setFailed(error.message);
+});
+
+
+async function runAction() {
+
+  // Can I leave this out? how to do this with sentryCli?
+  const tag = "test";
+
+  // This input is defined in action.yml and its value is provided by users of this Action in their own workflow files
+  const environment = core.getInput('environment');
+  core.info(`Environment is: ${environment}`);
+  console.log(`Environment is: ${environment}`);
+
+
+  const sentryCli = new SentryCli();
+
+  // Set commits
+  await sentryCli.releases.setCommits(tag, {
+    repo: 'repo',
+    auto: true
+  });
+
+  // Finalize the release
+  await sentryCli.releases.finalize(tag);
+}
+
